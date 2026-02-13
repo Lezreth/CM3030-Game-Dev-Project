0962a0621d4053fc9bbebfdd3476f499857e680a
Joe Anthony <122462684+Oraclej27@users.noreply.github.com>
2026-02-14 01:26:35 +0800

    Adding Scene Transition Audio

--- Finding-Primrose/Assets/Scripts/SceneTransitionController.cs
+++ Finding-Primrose/Assets/Scripts/SceneTransitionController.cs
@@ -6,6 +6,25 @@ public class SceneTransitionController : MonoBehaviour
 {
     public Animator fadeAnimator;
 
+    [Header("Audio")]
+    public AudioSource audioSource;
+    public AudioClip transitionSound;
+
+    private static SceneTransitionController instance;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);  
+        }
+        else
+        {
+            Destroy(gameObject);  
+        }
+    }
+
     public void StartSceneTransition(string sceneName)
     {
         Debug.Log("Starting transition to scene: " + sceneName);
@@ -16,6 +35,12 @@ public class SceneTransitionController : MonoBehaviour
     {
         Debug.Log($"Fading out scene transition for: {sceneName}");
         Debug.Log("Fade OUT start");
+
+        if (transitionSound != null && audioSource != null)
+        {
+            audioSource.PlayOneShot(transitionSound);
+        }
+
         fadeAnimator.SetTrigger("Out");
 
         yield return new WaitForSeconds(1f);
