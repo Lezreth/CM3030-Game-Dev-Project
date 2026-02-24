89759e628659770e85f66146dbc43b2583bcb59a
Joe Anthony <122462684+Oraclej27@users.noreply.github.com>
2026-02-24 16:40:16 +0800

    Orthographic camera view

--- Finding-Primrose/Assets/Scripts/CameraChoiceZoom.cs
+++ Finding-Primrose/Assets/Scripts/CameraChoiceZoom.cs
@@ -1,67 +1,4 @@
-//Reframes the scene for Choice UI interaction
-using UnityEngine;
-
-public class CameraController : MonoBehaviour
-{
-    public static CameraController Instance;
-
-    [Header("Movement")]
-    [SerializeField] float focusSpeed = 5f;
-    [SerializeField] float arriveThreshold = 0.05f;
-
-    private Transform target;
-    private bool isFocusing = false;
-
-    void Awake()
-    {
-        Instance = this;
-    }
-
-    void LateUpdate()
-    {
-        if (target == null) return;
-
-        Vector3 desiredPos = new Vector3(
-            target.position.x,
-            target.position.y,
-            transform.position.z
-        );
-
-        transform.position = Vector3.Lerp(
-            transform.position,
-            desiredPos,
-            Time.deltaTime * focusSpeed
-        );
-    }
-
-    public void FocusOn(Transform focusTarget)
-    {
-        target = focusTarget;
-        isFocusing = true;
-    }
-
-    public void ClearFocus()
-    {
-        target = null;
-        isFocusing = false;
-    }
-
-    public bool HasArrived()
-    {
-        if (!isFocusing || target == null)
-            return true;
-
-        Vector3 desiredPos = new Vector3(
-            target.position.x,
-            target.position.y,
-            transform.position.z
-        );
-
-        return Vector3.Distance(transform.position, desiredPos) < arriveThreshold;
-    }
-}
-
-// This version was made for Orthographic camera view. I will keep for now it in case we switch back to it. 
+////Reframes the scene for Choice UI interaction
 //using UnityEngine;
 
 //public class CameraController : MonoBehaviour
@@ -72,44 +9,29 @@ public class CameraController : MonoBehaviour
 //    [SerializeField] float focusSpeed = 5f;
 //    [SerializeField] float arriveThreshold = 0.05f;
 
-//    [Header("Zoom")]
-//    [SerializeField] float focusedSize = 4f;
-//    //private float defaultSize;
-
 //    private Transform target;
-//    private Camera cam;
 //    private bool isFocusing = false;
 
 //    void Awake()
 //    {
 //        Instance = this;
-//        cam = GetComponent<Camera>();
-//        //defaultSize = cam.orthographicSize;
 //    }
 
 //    void LateUpdate()
 //    {
-//        if (target != null)
-//        {
-//            Vector3 desiredPos = new Vector3(
-//                target.position.x,
-//                target.position.y,
-//                transform.position.z
-//            );
+//        if (target == null) return;
 
-//            transform.position = Vector3.Lerp(
-//                transform.position,
-//                desiredPos,
-//                Time.deltaTime * focusSpeed
-//            );
-//        }
+//        Vector3 desiredPos = new Vector3(
+//            target.position.x,
+//            target.position.y,
+//            transform.position.z
+//        );
 
-//        //float targetSize = isFocusing ? focusedSize : defaultSize;
-//        //cam.orthographicSize = Mathf.Lerp(
-//        //    cam.orthographicSize,
-//        //    targetSize,
-//        //    Time.deltaTime * focusSpeed
-//        //);
+//        transform.position = Vector3.Lerp(
+//            transform.position,
+//            desiredPos,
+//            Time.deltaTime * focusSpeed
+//        );
 //    }
 
 //    public void FocusOn(Transform focusTarget)
@@ -135,13 +57,91 @@ public class CameraController : MonoBehaviour
 //            transform.position.z
 //        );
 
-//        bool positionArrived =
-//            Vector3.Distance(transform.position, desiredPos) < arriveThreshold;
-
-//        //bool zoomArrived =
-//        //    Mathf.Abs(cam.orthographicSize - focusedSize) < arriveThreshold;
-
-//        //return positionArrived && zoomArrived;
-//        return positionArrived;
+//        return Vector3.Distance(transform.position, desiredPos) < arriveThreshold;
 //    }
 //}
+
+// This version was made for Orthographic camera view. I will keep for now it in case we switch back to it. 
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public static CameraController Instance;
+
+    [Header("Movement")]
+    [SerializeField] float focusSpeed = 5f;
+    [SerializeField] float arriveThreshold = 0.05f;
+
+    [Header("Zoom")]
+    [SerializeField] float focusedSize = 4f;
+    //private float defaultSize;
+
+    private Transform target;
+    private Camera cam;
+    private bool isFocusing = false;
+
+    void Awake()
+    {
+        Instance = this;
+        cam = GetComponent<Camera>();
+        //defaultSize = cam.orthographicSize;
+    }
+
+    void LateUpdate()
+    {
+        if (target != null)
+        {
+            Vector3 desiredPos = new Vector3(
+                target.position.x,
+                target.position.y,
+                transform.position.z
+            );
+
+            transform.position = Vector3.Lerp(
+                transform.position,
+                desiredPos,
+                Time.deltaTime * focusSpeed
+            );
+        }
+
+        //float targetSize = isFocusing ? focusedSize : defaultSize;
+        //cam.orthographicSize = Mathf.Lerp(
+        //    cam.orthographicSize,
+        //    targetSize,
+        //    Time.deltaTime * focusSpeed
+        //);
+    }
+
+    public void FocusOn(Transform focusTarget)
+    {
+        target = focusTarget;
+        isFocusing = true;
+    }
+
+    public void ClearFocus()
+    {
+        target = null;
+        isFocusing = false;
+    }
+
+    public bool HasArrived()
+    {
+        if (!isFocusing || target == null)
+            return true;
+
+        Vector3 desiredPos = new Vector3(
+            target.position.x,
+            target.position.y,
+            transform.position.z
+        );
+
+        bool positionArrived =
+            Vector3.Distance(transform.position, desiredPos) < arriveThreshold;
+
+        //bool zoomArrived =
+        //    Mathf.Abs(cam.orthographicSize - focusedSize) < arriveThreshold;
+
+        //return positionArrived && zoomArrived;
+        return positionArrived;
+    }
+}
