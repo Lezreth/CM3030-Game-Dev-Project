665ae0374aae08323db6b0c75fe5b7d5aae90b1f
Joe Anthony <122462684+Oraclej27@users.noreply.github.com>
2026-03-07 10:33:56 +0800

    Cleaned up one of my scripts

--- Finding-Primrose/Assets/Scripts/SceneTransitionController.cs
+++ Finding-Primrose/Assets/Scripts/SceneTransitionController.cs
@@ -1,51 +1,3 @@
-
-using System.Collections;
-using UnityEngine;
-using UnityEngine.SceneManagement;
-using System.Collections.Generic;
-
-// public class SceneTransitionController : MonoBehaviour
-// {
-//     public Animator fadeAnimator;
-
-//     [Header("Audio")]
-//     public AudioSource audioSource;
-//     public AudioClip transitionSound;
-
-//     [Header("NPC Audio Finish Before Changing")]
-//     [SerializeField] private List<AudioSource> npcAudios = new List<AudioSource>();
-
-
-//    public void RegisterNPCAudio(AudioSource audio)
-//     {
-//         if (audio != null && !npcAudios.Contains(audio))
-//             npcAudios.Add(audio);
-//     }
-
-//     public void StartSceneTransition(string sceneName)
-//     {
-//         StartCoroutine(TransitionRoutine(sceneName));
-//     }
-
-//     IEnumerator TransitionRoutine(string sceneName)
-//     {
-//         // Wait for all NPC audio to finish before fading
-//         yield return new WaitWhile(() => npcAudios.Exists(a => a != null && a.isPlaying));
-
-//         if (transitionSound != null && audioSource != null)
-//             audioSource.PlayOneShot(transitionSound);
-
-//         fadeAnimator.SetTrigger("Out");
-//         yield return new WaitForSeconds(1f);
-
-//         SceneManager.LoadScene(sceneName);
-
-//         yield return null;
-//         fadeAnimator.SetTrigger("In");
-//     }
-// }
-
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -75,7 +27,7 @@ public class SceneTransitionController : MonoBehaviour
 
     IEnumerator TransitionRoutine(string sceneName)
     {
-        // Wait for all NPC sequences to finish
+        
         yield return new WaitWhile(() => npcManagers.Exists(n => n != null && n.IsPlaying()));
 
         if (transitionSound != null && audioSource != null)
